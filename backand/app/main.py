ea66151e620f7072559a41e9864e2f8e3fa1c929
Xz40 <103625819+Xz40@users.noreply.github.com>
2026-04-22 03:06:20 +0000

    port

--- backand/app/main.py
+++ backand/app/main.py
@@ -47,4 +47,4 @@ async def process_question(request: QueryRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
