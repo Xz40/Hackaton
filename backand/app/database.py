34dc31a8c6a5835006ed33c1704a2d9ab44f66ce
Xz40 <103625819+Xz40@users.noreply.github.com>
2026-04-22 05:40:34 +0000

    321231321321

--- backand/app/database.py
+++ backand/app/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "port": os.getenv("DB_PORT", "5432"),
-    "dbname": os.getenv("DB_NAME", "drivee"),
+    "dbname": os.getenv("DB_NAME", "postgres"),
     "user": os.getenv("DB_USER", "postgres"),
     "password": os.getenv("DB_PASSWORD", "postgres"),
 }
